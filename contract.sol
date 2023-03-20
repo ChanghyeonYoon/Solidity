8d31aafaee65a07a39f1dab191bd172fb45d1889
Changhyeon Yoon <hyeon120411@gmail.com>
2023-03-20 17:04:05 +0900

    Feat : Lesson 1

--- contract.sol
+++ contract.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.4;
 
 contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -11,7 +14,19 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string _name, uint _dna) {
-
+    function _createZombie(string _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        NewZombie(id, _name, _dna);
     }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
 }
